904114316356644c4f1076699e74cca2c3de67df
Maite Sanchez Fornaris <maite.fornaris@gmail.com>
2021-05-25 18:27:40 -0400

    arregle los StringIndexer y la forma de leer el dataframe del archivo

--- src/main/java/com/mycompany/logisticregression/CylinderBandsAnalysis.java
+++ src/main/java/com/mycompany/logisticregression/CylinderBandsAnalysis.java
@@ -47,7 +47,7 @@ public class CylinderBandsAnalysis {
         // d. Aplique las transformaciones necesarias sobre los datos que contengan valores nominales, mediante técnicas de extracción de características.
 
         // Cargar el dataset, luego eliminar los valores nulos y convertir todas las variables nominales a numerica mediante codificacion
-        Dataset<Row> df = spark.read().option("header", true).option("inferSchema", "true").csv("/home/maite/Documents/LogisticRegression/src/main/java/com/mycompany/logisticregression/bands.data");
+        Dataset<Row> df = spark.read().option("header", false).option("inferSchema", "true").csv("/home/maite/Documents/LogisticRegression/src/main/java/com/mycompany/logisticregression/bands.data");
         
         df.printSchema();  // inciso a
         df.show(10);       // inciso a
@@ -56,69 +56,127 @@ public class CylinderBandsAnalysis {
         // Eliminar las filas con valores nulos en el dataframe
         
         
-        Dataset<Row> logregdataall = df.select(col("grain_screened"), 
-                col("ink_color"), col("proof_ctd_ink"), col("blade_mfg"), 
-                col("cylinder_division"), col("paper_type"), col("ink_type"),
-                col("direct_steam"), col("solvent_type"), col("type_on_cylinder"),
-                col("press_type"), col("press"), col("unit_number"),
-                col("cylinder_size"), col("paper_mill_location"), col("plating_tank"),
-                col("proof_cut"), col("viscosity"), col("caliper"), col("ink_temperature"),
-                col("humifity"), col("roughness"), col("blade_pressure"), col("varnish_pct"),
-                col("press_speed"), col("ink_pct"), col("solvent_pct"), col("esa_voltage"),
-                col("esa_amperage"), col("wax"), col("hardener"), col("roller_durometer"),
-                col("current_density"), col("anode_space_ratio"), col("chrome_content"),
-                col("band_type"));
-        
-        logregdataall.printSchema();
-        
-        Dataset<Row> logregdata = logregdataall.na().drop(); // inciso c
-        System.out.println("Cantidad de filas del dataframe despues de eliminar valores nulos: " + logregdata.count());
+        df = df.na().drop(); // inciso c
+        System.out.println("Cantidad de filas del dataframe despues de eliminar valores nulos: " + df.count());
         
         //Mostrar los posibles valores que puede tomar el campo objetivo (target) que vamos a predecir, en este caso es band_type
-        logregdata.groupBy("band_type").count().show();    // inciso b
+        df.groupBy("_c39").count().show();    // inciso b
         
         // Transformar los datos que tengas valores nominales, usar tecnicas de extraccion de caracteristicas
         
         
-        StringIndexer grain_screenedIndexer = new StringIndexer().setInputCol("grain_screened").setOutputCol("grain_screenedIndex");
-        StringIndexer ink_colorIndexer = new StringIndexer().setInputCol("ink_color").setOutputCol("ink_colorIndex");
-        StringIndexer proof_ctd_inkIndexer = new StringIndexer().setInputCol("proof_ctd_ink").setOutputCol("proof_ctd_inkIndex");
-        StringIndexer blade_mfgIndexer = new StringIndexer().setInputCol("blade_mfg").setOutputCol("blade_mfgIndex");
-        StringIndexer cylinder_divisionIndexer = new StringIndexer().setInputCol("cylinder_division").setOutputCol("cylinder_divisionIndex");
-        StringIndexer paper_typeIndexer = new StringIndexer().setInputCol("paper_type").setOutputCol("paper_typeIndex");
-        StringIndexer ink_typeIndexer = new StringIndexer().setInputCol("ink_type").setOutputCol("ink_typeIndex");
-        StringIndexer direct_steamIndexer = new StringIndexer().setInputCol("direct_steam").setOutputCol("direct_steamIndex");
-        StringIndexer solvent_typeIndexer = new StringIndexer().setInputCol("solvent_type").setOutputCol("solvent_typeIndex");
-        StringIndexer type_on_cylinderIndexer = new StringIndexer().setInputCol("type_on_cylinder").setOutputCol("type_on_cylinderIndex");
-        StringIndexer press_typeIndexer = new StringIndexer().setInputCol("press_type").setOutputCol("press_typeIndex");
-        StringIndexer pressIndexer = new StringIndexer().setInputCol("press").setOutputCol("pressIndex");
-        StringIndexer unit_numberIndexer = new StringIndexer().setInputCol("unit_number").setOutputCol("unit_numberIndex");
-        StringIndexer cylinder_sizeIndexer = new StringIndexer().setInputCol("cylinder_size").setOutputCol("cylinder_sizeIndex");
-        StringIndexer paper_mill_locationIndexer = new StringIndexer().setInputCol("paper_mill_location").setOutputCol("paper_mill_locationIndex");
-        StringIndexer plating_tankIndexer = new StringIndexer().setInputCol("plating_tank").setOutputCol("plating_tankIndex");
-        StringIndexer band_typeIndexer = new StringIndexer().setInputCol("band_type").setOutputCol("band_typeIndex");
         
-        OneHotEncoderEstimator encoder = new OneHotEncoderEstimator().setInputCols(new String[]{"grain_screenedIndex", "ink_colorIndex", "proof_ctd_inkIndex",
-            "blade_mfgIndex", "cylinder_divisionIndex", "paper_typeIndex", "ink_typeIndex", "direct_steamIndex", "solvent_typeIndex", "type_on_cylinderIndex",
-            "press_typeIndex", "pressIndex", "unit_numberIndex", "cylinder_sizeIndex", "paper_mill_locationIndex", "plating_tankIndex"}
-            ).setOutputCols(new String[]{"grain_screenedVec", "ink_colorVec", "proof_ctd_inkVec", "blade_mfgVec", "cylinder_divisionVec", "paper_typeVec",
-                "ink_typeVec", "direct_steamVec", "solvent_typeVec", "type_on_cylinderVec", "press_typeVec", "pressVec", "unit_numberVec", "cylinder_sizeVec",
-                "paper_mill_locationVec", "plating_tankVec"});
+        StringIndexer grain_screenedIndexer = new StringIndexer().setInputCol("_c4").setOutputCol("grain_screenedIndex");                 //1
+        StringIndexer ink_colorIndexer = new StringIndexer().setInputCol("_c5").setOutputCol("ink_colorIndex");                           //2
+        StringIndexer proof_ctd_inkIndexer = new StringIndexer().setInputCol("_c6").setOutputCol("proof_ctd_inkIndex");                   //3
+        StringIndexer blade_mfgIndexer = new StringIndexer().setInputCol("_c7").setOutputCol("blade_mfgIndex");                           //4
+        StringIndexer cylinder_divisionIndexer = new StringIndexer().setInputCol("_c8").setOutputCol("cylinder_divisionIndex");           //5
+        StringIndexer paper_typeIndexer = new StringIndexer().setInputCol("_c9").setOutputCol("paper_typeIndex");                        //6
+        StringIndexer ink_typeIndexer = new StringIndexer().setInputCol("_c10").setOutputCol("ink_typeIndex");                              //7
+        StringIndexer direct_steamIndexer = new StringIndexer().setInputCol("_c11").setOutputCol("direct_steamIndex");                  //8
+        StringIndexer solvent_typeIndexer = new StringIndexer().setInputCol("_c12").setOutputCol("solvent_typeIndex");                  //9
+        StringIndexer type_on_cylinderIndexer = new StringIndexer().setInputCol("_c13").setOutputCol("type_on_cylinderIndex");      //10
+        StringIndexer press_typeIndexer = new StringIndexer().setInputCol("_c14").setOutputCol("press_typeIndex");                        //11
+        StringIndexer pressIndexer = new StringIndexer().setInputCol("_c15").setOutputCol("pressIndex");                                       //12
+        StringIndexer unit_numberIndexer = new StringIndexer().setInputCol("_c16").setOutputCol("unit_numberIndex");                     //13
+        StringIndexer cylinder_sizeIndexer = new StringIndexer().setInputCol("_c17").setOutputCol("cylinder_sizeIndex");               //14
+        StringIndexer paper_mill_locationIndexer = new StringIndexer().setInputCol("_c18").setOutputCol("paper_mill_locationIndex"); //15
+        StringIndexer plating_tankIndexer = new StringIndexer().setInputCol("_c19").setOutputCol("plating_tankIndex");                  //16
+        StringIndexer proof_cutIndexer = new StringIndexer().setInputCol("_c20").setOutputCol("proof_cutIndex");                           //17
+        StringIndexer viscosityIndexer = new StringIndexer().setInputCol("_c21").setOutputCol("viscosityIndex");                           //18
+        StringIndexer caliperIndexer = new StringIndexer().setInputCol("_c22").setOutputCol("caliperIndex");                                 //19
+        StringIndexer ink_temperatureIndexer = new StringIndexer().setInputCol("_c23").setOutputCol("ink_temperatureIndex");         //20
+        StringIndexer humifityIndexer = new StringIndexer().setInputCol("_c24").setOutputCol("humifityIndex");                              //21
+        StringIndexer roughnessIndexer = new StringIndexer().setInputCol("_c25").setOutputCol("roughnessIndex");                           //22
+        StringIndexer blade_pressureIndexer = new StringIndexer().setInputCol("_c26").setOutputCol("blade_pressureIndex");            //23
+        StringIndexer varnish_pctIndexer = new StringIndexer().setInputCol("_c27").setOutputCol("varnish_pctIndex");                     //24
+        StringIndexer press_speedIndexer = new StringIndexer().setInputCol("_c28").setOutputCol("press_speedIndex");                     //25
+        StringIndexer ink_pctIndexer = new StringIndexer().setInputCol("_c29").setOutputCol("ink_pctIndex");                                 //26
+        StringIndexer solvent_pctIndexer = new StringIndexer().setInputCol("_c30").setOutputCol("solvent_pct");                          //27
+        StringIndexer esa_voltageIndexer = new StringIndexer().setInputCol("_c31").setOutputCol("esa_voltageIndex");                     //28
+        StringIndexer esa_amperageIndexer = new StringIndexer().setInputCol("_c32").setOutputCol("esa_amperageIndex");                  //29
+        StringIndexer waxIndexer = new StringIndexer().setInputCol("_c33").setOutputCol("waxIndex");                                             //30
+        StringIndexer hardenerIndexer = new StringIndexer().setInputCol("_c34").setOutputCol("hardenerIndex");                              //31
+        StringIndexer roller_durometerIndexer = new StringIndexer().setInputCol("_c35").setOutputCol("roller_durometerIndex");      //32
+        StringIndexer current_densityIndexer = new StringIndexer().setInputCol("_c36").setOutputCol("current_densityIndex");         //33
+        StringIndexer anode_space_ratioIndexer = new StringIndexer().setInputCol("_c37").setOutputCol("anode_space_ratioIndex");   //34
+        StringIndexer chrome_contentIndexer = new StringIndexer().setInputCol("_38").setOutputCol("chrome_contentIndex");            //35
+        
+        // Discretizar tambien la salida
+        StringIndexer band_typeIndexer = new StringIndexer().setInputCol("_c39").setOutputCol("band_typeIndex");                           // label codification
+        System.out.println(grain_screenedIndexer);
+        OneHotEncoderEstimator encoder = new OneHotEncoderEstimator().setInputCols(new String[]{"grain_screenedIndex",
+            "ink_colorIndex", "proof_ctd_inkIndex", "blade_mfgIndex", "cylinder_divisionIndex", "paper_typeIndex",
+            "ink_typeIndex", "direct_steamIndex", "solvent_typeIndex", "type_on_cylinderIndex", "press_typeIndex",
+            "pressIndex", "unit_numberIndex", "cylinder_sizeIndex", "paper_mill_locationIndex", "plating_tankIndex",
+            "proof_cutIndex", "viscosityIndex", "caliperIndex", "ink_temperatureIndex", "humifityIndex", "roughnessIndex",
+            "blade_pressureIndex", "varnish_pctIndex", "press_speedIndex", "ink_pctIndex", "solvent_pctIndex", "esa_voltageIndex",
+            "esa_amperageIndex", "waxIndex", "hardenerIndex", "roller_durometerIndex", "current_densityIndex", "anode_space_ratioIndex",
+            "chrome_contentIndex"})
+                .setOutputCols(new String[]{"grain_screenedVec", "ink_colorVec", "proof_ctd_inkVec", "blade_mfgVec", "cylinder_divisionVec",
+                    "paper_typeVec", "ink_typeVec", "direct_steamVec", "solvent_typeVec", "type_on_cylinderVec", "press_typeVec", "pressVec",
+                    "unit_numberVec", "cylinder_sizeVec", "paper_mill_locationVec", "plating_tankVec", "proof_cutVec", "viscosityVec",
+                    "caliperVec", "ink_temperatureVec", "humifityVec", "roughnessVec", "blade_pressureVec", "varnish_pctVec",
+                    "press_speedVec", "ink_pctVec", "solvent_pctVec", "esa_voltageVec", "esa_amperageVec", "waxVec", "hardenerVec",
+                    "roller_durometerVec", "current_densityVec", "anode_space_ratioVec", "chrome_contentVec"});
        
         //Hacer 
         // 2. Seleccione al menos tres algoritmos de aprendizaje automático de acuerdo al problema identificado en el dataset y realice las siguientes acciones:
+        // 1er modelo: Multilayer perceptron
+        // 2do modelo: Random forest classifier
+        // 3er modelo: matrix factorization
         // a. Cree un VectorAssembler a partir de los datos pre-procesados y divida de forma aleatoria el conjunto en dos partes un 70 % para entrenamiento y el 30 % para pruebas.
-        VectorAssembler assembler = new VectorAssembler().setInputCols(new String[]{"grain_screened", "ink_color", "proof_ctd_ink",
-            "blade_mfg", "cylinder_division", "paper_type", "ink_type", "direct_steam", "solvent_type", "type_on_cylinder",
-            "press_type", "press", "unit_number", "cylinder_size", "paper_mill_location", "plating_tank",
-            "proof_cut", "viscosity", "caliper", "ink_temperature", "humifity", "roughness", "blade_pressure", "varnish_pct", 
-            "press_speed", "ink_pct", "solvent_pct", "esa_voltage", "esa_amperage", "wax", "hardener", "roller_durometer", 
-            "current_density", "anode_space_ratio", "chrome_content"}).setOutputCol("features");
+        //VectorAssembler assembler = new VectorAssembler().setInputCols(new String[]{"grain_screened", "ink_color", "proof_ctd_ink", "blade_mfg",
+        //    "cylinder_division", "paper_type", "ink_type", "direct_steam", "solvent_type", "type_on_cylinder", "press_type", "press", "unit_number",
+        //    "cylinder_size", "paper_mill_location", "plating_tank", "proof_cut", "viscosity", "caliper", "ink_temperature", "humifity", "roughness",
+        //    "blade_pressure", "varnish_pct", "press_speed", "ink_pct", "solvent_pct", "esa_voltage", "esa_amperage", "wax", "hardener", "roller_durometer",
+        //    "current_density", "anode_space_ratio", "chrome_content"}).setOutputCol("features");
         
-        Dataset<Row>[] split = logregdataall.randomSplit(new double[]{0.8, 0.2}, 1);
-        split[0].show(10);
+        //Dataset<Row>[] split = logregdataall.randomSplit(new double[]{0.8, 0.2}, 1);
+        //split[0].show(10);
+        
         
         // b. Entrene cada modelo elegido con dicha entrada y ajuste los hiper-parámetros correspondientes de forma automática.
+        //LogisticRegression lr = new LogisticRegression();
+        
+        //Pipeline pipeline = new Pipeline().setStages(new PipelineStage[]{grain_screenedIndexer, ink_colorIndexer, proof_ctd_inkIndexer,
+        //    blade_mfgIndexer, cylinder_divisionIndexer, paper_typeIndexer, ink_typeIndexer, direct_steamIndexer, solvent_typeIndexer,
+        //    type_on_cylinderIndexer, press_typeIndexer, pressIndexer, unit_numberIndexer, cylinder_sizeIndexer, paper_mill_locationIndexer,
+        //    plating_tankIndexer, proof_cutIndexer, viscosityIndexer, caliperIndexer, ink_temperatureIndexer, humifityIndexer, roughnessIndexer,
+        //    blade_pressureIndexer, varnish_pctIndexer, press_speedIndexer, ink_pctIndexer, solvent_pctIndexer, esa_voltageIndexer, esa_amperageIndexer,
+        //    waxIndexer, hardenerIndexer, roller_durometerIndexer, current_densityIndexer, anode_space_ratioIndexer, chrome_contentIndexer, lr});
+        
+        
+        // Perceptron multicapa, aun no funciona
+        //int layers[] = new int[]{35, 70, 35, 2};
+        //MultilayerPerceptronClassifier mlp = new MultilayerPerceptronClassifier().setLayers(layers).setBlockSize(128).setSeed(1).setMaxIter(100);
+        //mlp.setFeaturesCol("featuresNormalized");
+        //mlp.setLabelCol("band_typeIndex");
+        
+        //Normalizer normalizer = new Normalizer().setInputCol("features").setOutputCol("featuresNormalized").setP(1.0);
+        
+        //System.out.println("schema\n\n" + split[0].schema());
+        //System.out.println("schema\n\n" + split[0].schema().json());
+        
+        //Pipeline pipelineMLP = new Pipeline().setStages(new PipelineStage[]{band_typeIndexer, assembler, normalizer, mlp});
+        
+        // Configurar un grid para buscar hiperparametros
+        //ParamGridBuilder paramGridMLP = new ParamGridBuilder();
+        //paramGridMLP.addGrid(mlp.stepSize(), new double[]{0.01, 0.001, 0.0015});
+        
+        //TrainValidationSplit trainValSplitMLP = new TrainValidationSplit().setEstimator(pipelineMLP).setEstimatorParamMaps(paramGridMLP.build())
+        //        .setEvaluator(new BinaryClassificationEvaluator());
+        
+        //TrainValidationSplitModel modelMLP = trainValSplitMLP.fit(split[0]);
+        //Dataset<Row> resultsMLP = modelMLP.transform(split[1]);
+        //resultsMLP.show();
+        
+        //Calcular y mostrar las metricas de Accuracy y Confusion Matrix
+        //MulticlassMetrics metrics = new MulticlassMetrics(resultsMLP.select("prediction","band_typeIndex"));
+        
+        //System.out.println("Test set accuracy: " + metrics.accuracy());
+        //System.out.println("Confusion matrix: " + metrics.confusionMatrix());
+        
         // c. Evalúe el resultado del entrenamiento de cada algoritmo mediante el conjunto de pruebas. Muestre su accuracy y matriz de confusión.
         // d. Salve en un fichero el modelo que mejor resultados arrojó.
         
